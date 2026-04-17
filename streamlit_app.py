9d1ac22695c5cf7243682b7d8a3beaa272367947
DarkKnight1776 <whitehorse1776@proton.me>
2026-04-17 13:35:48 -0500

    Added material from day 12 to 2 previous days

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,22 @@ options = st.multiselect(
     ['Green', 'Yellow', 'Red', 'Blue'],
     ['Yellow', 'Red'])
 
-st.write('You sselected:', options)
+st.write('You selected:', options)
+
+st.header('st.checkbox')
+
+st.write('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+    st.write("Great! Here's your ice cream.")
+
+if coffee:
+    st.write("Great! Here's your coffee.")
+
+if cola:
+    st.write("Great! Here's your cola.")
+
