8025f517614f14d49ffc7f6ed317d923c3e69a1b
DarkKnight1776 <whitehorse1776@proton.me>
2026-04-17 14:12:06 -0500

    Added day 17 and removed the rest

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,6 @@
 import streamlit as st
 
-st.title('Customizing the theme of Streamlit apps')
+st.title('st.secrets')
 
-st.write('Contents of the `.streamlit/config.toml` file of this app')
+st.write(st.secrets['message'])
 
-st.code("""
-[theme]
-primaryColor="#F39C12"
-backgroundColor="#2E86C1"
-secondaryBackgroundColor="C60AF0"
-textColor="#F0300A"
-font="monospace"
-""")
-
-number = st.sidebar.slider('Select a number:', 0, 10, 5)
-st.write('Selected number from slider widget is:', number)
